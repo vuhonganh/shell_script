51dbb6ec6821588929d12bb219b1b6db75088bb2
Hong Anh VU <vuhonganh91@gmail.com>
2015-12-16 01:02:18 +0100

    improve: prepend function in prepend_test.sh : now work even if the original path is null

--- c1-shell-something-out/prepend_test.sh
+++ c1-shell-something-out/prepend_test.sh
@@ -3,11 +3,11 @@
 
 apath=/usr/bin
 
-
-
+#${parameter:+expression} will be expression if the parameter is set and not null
+#the version of prepend function below works even if the $1 is null
 prepend()
 {
-    [ -d $2 ] && eval $1=$2:\$$1 && export $1; 
+    [ -d $2 ] && eval $1=$2\$\{$1:+:\$$1\} && export $1; 
 }
 
 echo '$PATH' = $PATH
@@ -17,3 +17,8 @@ echo '$PATH' = $PATH
 echo '$apath' = $apath
 prepend apath /home/anhvh
 echo '$apath' = $apath
+
+nullpath=''
+echo '$nullpath' = $nullpath
+prepend nullpath /home/anhvh
+echo '$nullpath' = $nullpath
