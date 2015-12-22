d13fc0b02d5e75b42d8ee98f824477c347a2c1cb
hvu <vuhonganh91@gmail.com>
2015-12-22 16:30:00 +0000

    modify: flow_control_test.sh : add -a and -o option which stands for and, or in test square bracket

--- c1-shell-something-out/flow_control_test.sh
+++ c1-shell-something-out/flow_control_test.sh
@@ -7,8 +7,14 @@ printf "\e[1;33m%s\e[0m \e[1;35m%s\e[0m\n" "[ condition ] && command;" "#command
 printf "\e[1;33m%s\e[0m \e[1;35m%s\e[0m\n" "[ condition ] || command;" "#command will execute if condition is false, because if condition is true it will not go further to process command";
 
 i=0;
-[ $i -eq 0 ] && echo \$i is equal 0;
+[ $i -eq 0 ] && echo \$i is equal to 0;
 
-[ $i -eq 1 ] || echo \$i is not equal 1;
+[ $i -eq 1 ] || echo \$i is not equal to 1;
 
-[ $i -eq 0 ] || echo \$i is not equal 0;
+[ $i -eq 0 ] || echo \$i is not equal to 0;
+
+printf "\e[1;33m%s\e[0m \e[1;35m%s\e[0m\n" "[ cond1 -a cond2 ] && command;" "#command will execute if both cond1 and cond2 are true, -a stands for and"
+printf "\e[1;33m%s\e[0m \e[1;35m%s\e[0m\n" "[ cond1 -o cond2 ] || command;" "#command will execute if both cond1 and cond2 are false";
+j=1;
+[ $i -eq 0 -a $j -eq 1 ] && echo \$i is equal to 0 and \$j is equal to 1;
+[ $i -eq 1 -o $j -eq 0 ] || echo \$i is not equal to 1 and \$j is not equal to 0;
