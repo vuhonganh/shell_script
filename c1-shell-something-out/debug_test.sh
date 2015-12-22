916937ab4a839de72d31b17fce4e06182f9d18ab
hvu <vuhonganh91@gmail.com>
2015-12-22 10:51:11 +0000

    modify: debug_test.sh : add some commands and explain its functionality: set +x, set -x, set -v, set +v the two latters need to be on one line

--- c1-shell-something-out/debug_test.sh
+++ c1-shell-something-out/debug_test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #File: debug_test.sh : need to export _DEBUG=on ./script_name.sh to run this script in debug mode
 
+#NOTE some trick: 
+#set -x : start displaying arguments and commands upon their execution
+#set +x : stop displaying (disable) the effect of set -x
+#set -v : display input when they are read and almost every things in the script (stronger effect than set -x )
+#set +v : disable printing input
+
 function DEBUG()
 {
 	[ "${_DEBUG}" == "on" ] && $@ || :  # : tell the bash to do nothing
@@ -10,3 +16,9 @@ for i in {1..10}
 do
 	DEBUG echo $i
 done
+
+
+echo Enter password:;
+read password;
+set -v; set +v; #make these 2 commands in one line avoid printing unnecessary lines to stdout
+echo You have entered a password;
