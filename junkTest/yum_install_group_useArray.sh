95c037cf36008fba17d93368b5fd7077f5504558
hvu <vuhonganh91@gmail.com>
2016-01-08 14:41:57 +0100

    modify: junkTest/yum_install_group_useArray.sh : found the best way to do this and also a good way to use array

--- junkTest/yum_install_group_useArray.sh
+++ junkTest/yum_install_group_useArray.sh
@@ -1,17 +1,28 @@
 #!/bin/zsh -x
 #File : test_install_group.sh
-
 echo IFS = ${IFS};
-defaultIFS=${IFS};
 
-IFS="";
 array=("X Window System" "Fonts");
-for elem in ${array[@]}; do
-    #echo "\"${elem}\"";
-    #printf %s \"${elem}\"
-    #sudo yum groupinstall \"${elem}\"
-    #sudo yum groupinstall `echo "${elem}"` #WORK
-    sudo yum groupinstall "${elem}" #WORK TOO
-done
 
-IFS=${defaultIFS}
+echo "We have complicated the things, we can do like this"
+
+function bestway()
+{
+    for elem in $array; do
+        sudo yum groupinstall $elem
+    done
+}
+
+function oldwayIused()
+{
+    defaultIFS=${IFS};
+    IFS="";
+    for elem in ${array[@]}; do
+        #sudo yum groupinstall \"${elem}\" #WILL NOT WORK
+        #sudo yum groupinstall `echo "${elem}"` #WORK
+        sudo yum groupinstall "${elem}" #WORK TOO
+    done
+    IFS=${defaultIFS}
+}
+
+bestway
