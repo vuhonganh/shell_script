0097b1b7a2f1eaeb762947ecdfd0498d439dd6ce
hvu <vuhonganh91@gmail.com>
2016-01-08 09:35:59 +0100

    modify: junkTest/dirname_basename.sh : add some lines using sed and tr, lesson learned is sed is string-based replacemeng and tr is character-based replacement

--- junkTest/dirname_basename.sh
+++ junkTest/dirname_basename.sh
@@ -3,14 +3,19 @@ function test_dirname_basename()
     echo 
     echo "Finding behavior of dirname and basename:"
     echo 
-    for DEP in bms/2-5-0 bms/python_libs/1-0-0-0 ccache/3-1-6 oracle/10-2-0-3 oracle/10-2-0-4 python/2-7-6-0 toolchain/amd64_2_6_16/1-0-0-0; do
+    for DEP in bms/2-5-0 bms/python_libs/1-0-0.0 ccache/3-1-6 oracle/10-2-0-3 oracle/10-2-0-4 python/2-7-6-0 toolchain/amd64_2_6_16/1-0-0-0; do
 	#sync_component nomad/linux/`dirname $DEP` `basename $DEP`
 	echo "\${DEP} =  ${DEP}"
 	vardir=`dirname $DEP`
 	varbase=`basename $DEP`
 	echo "dirname \${DEP} is $vardir "
 	echo "basename \${DEP} is $varbase "
-	echo
+	#o duoi la bien :: thanh / ????
+	COMPONENT=`echo ${vardir} | sed 's|::|/|g'`
+	#VERSION=`echo ${varbase} | tr '.' '-'`
+	VERSION=`echo ${varbase} | sed 's/\./-/g'`
+	echo "COMPONENT = ${COMPONENT}"
+	echo "VERSION = ${VERSION}"
     done
     wait
     #unset COMPONENT VERSION
